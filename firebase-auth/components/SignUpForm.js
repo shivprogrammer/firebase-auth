59eea81448e53ab1fbac4c3bd6730d712290f7cc
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-08 22:33:27 -0800

    linking up SignUpForm to the back end of shivvy-one-time-password

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-shivvy-one-time-password.cloudfunctions.net';
 
 class SignUpForm extends Component {
   state = { phone: '' };
 
-  handleSubmit = () => {
 
+  handleSubmit = () => {
+    axios.post(`${ROOT_URL}/createUser`, {
+      phone: this.state.phone
+    })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+      });
   }
 
   render() {
