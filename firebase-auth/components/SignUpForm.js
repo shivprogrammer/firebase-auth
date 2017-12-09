5af0ef532e9c29b86499d441c52e7e9d7dae1a19
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-08 22:11:40 -0800

    styling and setting up default state for phone | SignUpForm

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -3,11 +3,15 @@ import { View } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+
   render() {
     return (
       <View>
-        <FormLabel>Enter Phone Number</FormLabel>
-        <FormInput />
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput />
+        </View>
         <Button title="Submit" />
       </View>
     );
