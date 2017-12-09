9efa20631eab2f1fac356a3c595967039ca31250
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-08 23:27:16 -0800

    Started SignInForm.js

---
+++ firebase-auth/components/SignInForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-shivvy-one-time-password.cloudfunctions.net';
+
+class SignInForm extends Component {
+  state = { phone: '' };
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit" />
+      </View>
+    );
+  }
+}
+
+export default SignInForm;
